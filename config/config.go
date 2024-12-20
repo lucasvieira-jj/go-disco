7cf7400d7969c94aaf10dc7acbc414f8aa9e85b0
Lucas Vieira <tilthyx@gmail.com>
2024-12-20 20:01:07 -0300

    Config: Adding the initial config of the project

---
+++ config/config.go
@@ -0,0 +1,8 @@
+package config
+
+var (
+	baseURL = "https://www.discogs.com/pt_BR"
+
+	// UserAgent : variable to avoid website blocks
+	UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+)
