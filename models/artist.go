d262fb74dff27183e6f46bb3bb861fe98515795a
Lucas Vieira <tilthyx@gmail.com>
2024-12-20 20:04:36 -0300

    Model: Create the artist struct for using in scraper

---
+++ models/artist.go
@@ -0,0 +1,23 @@
+package models
+
+type Artist struct {
+	Name     string   `json:"name"`
+	Genre    string   `json:"genre"`
+	Members  []string `json:"members"`
+	Websites []string `json:"websites"`
+	Albums   []Album  `json:"albums"`
+}
+
+type Album struct {
+	Name   string   `json:"name"`
+	Year   string   `json:"year"`
+	Record []string `json:"Record"`
+	Styles []string `json:"styles"`
+	Tracks []Track  `json:"tracks"`
+}
+
+type Track struct {
+	Number   int    `json:"number"`
+	Title    string `json:"title"`
+	Duration string `json:"duration"`
+}
